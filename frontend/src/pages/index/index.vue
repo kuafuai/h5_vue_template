108f587fba758be1871e4ed6d1d7d86e6ec5b2df
Yake <jiangfei@kuafuai.net>
2024-09-20 15:41:07 +0800

    #feat(布局)：pc布局

---
+++ frontend/src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  11
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+::v-deep .uni-left-window {
+  height: 100% ;
+}
+</style>
--- frontend/src/responsive/leftwin.vue
+++ frontend/src/responsive/leftwin.vue
@@ -1,9 +1,54 @@
 <template>
-    <view>
-        1111
+  <scroll-view style="height: 100%;" scroll-y="true">
+
+    <view class="flex-c-between-start h-full">
+
+      <view class="w-full">
+        <view class="flex-start-center m-30 ">
+          <img src="@/assets/logo/logo.png" width="32px" height="34px"/>
+          <span class="m-l-10" style="font-weight: bold; font-size: 24px;">CodeFlying</span>
+        </view>
+
+        <view class="flex-start-center m-y-30 m-l-22">
+          <h3 class="m-l-5">{{ left_title }}</h3>
+        </view>
+      </view>
+
+      <view class="h-full w-full text-left m-l-10">
+        <template v-for="item in dynamicRoutes" :key="item.path">
+          <navigator v-if="!item.meta.shownot" :url="item.path" open-type="navigate" hover-class="navigator-hover"
+                     @click="closeDrawer">
+            <text type="default">{{ item.meta.title }}</text>
+          </navigator>
+        </template>
+      </view>
+
+
+      <view class="w-full m-b-30">
+        <view class="flex-end-center m-r-20">
+          <image src="@/assets/logo/logo.png" style="width: 30px; height: 30px;" mode="aspectFill">
+          </image>
+
+          <uni-badge :is-dot="true" :text="1" absolute="rightTop" class="item m-l-20">
+            <uni-icons type="chatbubble" size="30"></uni-icons>
+          </uni-badge>
+        </view>
+      </view>
+
     </view>
+
+  </scroll-view>
 </template>
 
 <script setup>
 
+
+import {dynamicRoutes} from '@/router/dynamic';
+
+const {proxy} = getCurrentInstance();
+
+let left_title = import.meta.env.VITE_APP_NAME;
+
+
 </script>
+

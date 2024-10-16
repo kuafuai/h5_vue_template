1f2f75d865e96a6f8b266e37515567650cd6472a
kuafu-limanxiang <limanxiang@kuafuai.net>
2024-10-16 23:00:13 +0800

    fix/lmx_safety: update 静态资源与分类表的建表语句

--- backend/src/main/resources/db/mysql/V1.0.0__init.sql
+++ backend/src/main/resources/db/mysql/V1.0.0__init.sql
@@ -1,44 +1,30 @@
 show tables;
-drop table if exists static_resources;
-create table static_resources
+DROP TABLE IF EXISTS static_resources;
+CREATE TABLE IF NOT EXISTS static_resources
 (
-    resource_id              int
-        primary key autoincrement,
-    resource_name            VARCHAR(255) not null,
-    resource_path            VARCHAR(500) not null,
-    related_table_name       VARCHAR(255),
-    related_table_key        int,
-    relate_table_column_name varchar(255)
-);
-
-
-
-drop table if exists category;
-create table if not exists category
-(
-    id                     integer primary key autoincrement, -- id
-    name                   varchar(255) not null,             -- 列别名称
-    parent_id              integer      not null,             -- 父级类别id
-    create_time            date,
-    update_time            date,
-    relevance_table        varchar(255),                      -- 表分类
-    relevance_table_column varchar(255)                       -- 分类字段
+    resource_id              INT PRIMARY KEY AUTO_INCREMENT, -- 设置为自增
+    resource_name            VARCHAR(255) NOT NULL,
+    resource_path            VARCHAR(500) NOT NULL,
+    related_table_name       VARCHAR(255) DEFAULT NULL,
+    related_table_key        INT DEFAULT NULL,
+    relate_table_column_name VARCHAR(255) DEFAULT NULL
     );
 
 
 
 
-drop table if exists category;
-create table if not exists category
+DROP TABLE IF EXISTS category;
+CREATE TABLE IF NOT EXISTS category
 (
-    id                     integer primary key autoincrement, -- id
-    name                   varchar(255) not null,             -- 列别名称
-    parent_id              integer      not null,             -- 父级类别id
-    create_time            date,
-    update_time            date,
-    relevance_table        varchar(255),                      -- 表分类
-    relevance_table_column varchar(255)                       -- 分类字段
+    id                     INT PRIMARY KEY AUTO_INCREMENT, -- id
+    name                   VARCHAR(255) NOT NULL,          -- 类别名称
+    parent_id              INT DEFAULT NULL,               -- 父级类别id
+    create_time            DATETIME DEFAULT CURRENT_TIMESTAMP, -- 创建时间
+    update_time            DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 更新时间
+    relevance_table        VARCHAR(255) DEFAULT NULL,      -- 表分类
+    relevance_table_column VARCHAR(255) DEFAULT NULL       -- 分类字段
     );
+
 
 
 

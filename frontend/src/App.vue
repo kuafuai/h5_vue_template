eb08fa4ff96e3ce71bc3a90b2b4c5bc538623e53
allen <993013755@qq.com>
2024-11-12 14:51:16 +0800

    feat：禁止穿透、禁止双击放大

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,17 @@ console.log("stepup")
 
 <style>
 /*每个页面公共css */
- ::v-deep .uni-popup__wrapper{
-    width: 80%;
+::v-deep .uni-popup__wrapper{
+  width: 80%;
+}
+/* 禁止透传 */
+::v-deep uni-popup{
+  z-index: 99999 !important;
+}
+html, body {
+  touch-action: manipulation; /* 禁用双击放大 */
+}
+page{
+  height: 100vh !important;
 }
 </style>

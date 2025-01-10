cd1b7cf0bf2f1b928e289efb915a4c1b860e3736
allen <993013755@qq.com>
2025-01-10 20:44:10 +0800

    feat(chatbot): 放开chatbot接口

--- backend/src/main/java/com/kuafu/login/config/SecurityConfig.java
+++ backend/src/main/java/com/kuafu/login/config/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         registry
                 .antMatchers("/doc.html").permitAll()
                 .antMatchers("/login/**").permitAll()
+                .antMatchers("/chatbot/**").permitAll()
                 .antMatchers("/common/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/*.html", "/**/*.html", "/**/*.css", "/**/*.js", "/profile/**").permitAll()
                 .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll();

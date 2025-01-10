fd6b24a5304068d1e5b981aa08efc504332880a2
allen <993013755@qq.com>
2025-01-10 10:43:38 +0800

    fix(chatbot): 去除导报报错

--- backend/src/main/java/com/kuafu/web/dify/ConfigService.java
+++ backend/src/main/java/com/kuafu/web/dify/ConfigService.java
@@ -3,8 +3,6 @@ package com.kuafu.web.dify;
 import com.kuafu.common.domin.ErrorCode;
 import com.kuafu.common.exception.BusinessException;
 import com.kuafu.dify.constant.DifyConstant;
-import com.kuafu.web.entity.DifyConfig;
-import com.kuafu.web.service.IDifyConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 

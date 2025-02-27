79102471f45d18016e96bbd1fcb44b4d9ab78ee4
kuafu-limanxiang <limanxiang@kuafuai.net>
2025-02-27 16:07:13 +0800

    fix: 添加 base-load-more 组件

---
+++ frontend/src/components/base-load-more.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="load-more">
+    <text @click="click">{{ text }}</text>
+  </view>
+</template>
+
+<script setup>
+
+import {defineProps} from "vue";
+
+
+const emits = defineEmits(['click'])
+const click = (item) => {
+  emits('click',item)
+}
+const props = defineProps({
+  text: {
+    type: String,
+    default: '查看更多',
+  }
+});
+</script>
+
+<style scoped>
+.load-more {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+ }
+.load-more text{
+  color: #5D5FEF;
+  margin-right: 4%;
+}
+</style>

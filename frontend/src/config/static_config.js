0c729d02f2ac4feff13978941824f83a99c4ee45
kuafu-limanxiang <limanxiang@kuafuai.net>
2025-03-21 18:58:40 +0800

    feat: 添加默认选项

--- frontend/src/config/static_config.js
+++ frontend/src/config/static_config.js
@@ -1,5 +1,9 @@
-const STATIC_URL= import.meta.env.VITE_STATIC_URL
-const get_resource_url=(path)=>{
-    return STATIC_URL+path
+let STATIC_URL = import.meta.env.VITE_STATIC_URL
+if (STATIC_URL === undefined || STATIC_URL === '' || STATIC_URL == null) {
+    STATIC_URL = 'https://kuafuai.obs.cn-east-3.myhuaweicloud.com/h5_vue_template/'
+}
+const get_resource_url = (path) => {
+    if (STATIC_URL)
+        return STATIC_URL + path
 }
 export default get_resource_url
